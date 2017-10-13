654372849cf537f709b789a31e1048e6d9971470
Davide Gallitelli <davidegallitelli@gmail.com>
2017-10-13 10:42:50 +0200

    Database class

---
+++ Database.cpp
@@ -0,0 +1,63 @@
+#include "Database.h"
+
+Database::Database()
+{
+}
+
+shared_ptr<PhotoObject> Database::newPhoto(string _name, string _path, float _lat, float _longit)
+{
+    shared_ptr<PhotoObject> p(new PhotoObject(_name, _path, _lat, _longit));
+    mObjMap[_name] = p;
+    return p;
+}
+
+shared_ptr<VideoObject> Database::newVideo(string _name, string _path, int _duration)
+{
+    shared_ptr<VideoObject> v(new VideoObject(_name, _path, _duration));
+    mObjMap[_name] = v;
+    return v;
+}
+
+shared_ptr<Film> Database::newFilm(string _name, string _path, int _duration, int _nChs, int *_chList)
+{
+   shared_ptr<Film> f(new Film(_name, _path, _duration, _nChs, _chList));
+   mObjMap[_name] = f;
+   return f;
+}
+
+shared_ptr<Group> Database::newGroup(string _name)
+{
+    shared_ptr<Group> g(new Group(_name));
+    groupMap[_name] = g;
+    return g;
+}
+
+void Database::reproduceFromName(string name)
+{
+    auto it=mObjMap.find(name);
+    if (it == mObjMap.end()){
+        cout << "No multimedia object found with that name." << endl;
+    } else {
+        (it->second)->reproduce();
+    }
+}
+
+void Database::printObjectFromName(string name)
+{
+    auto it=mObjMap.find(name);
+    if (it == mObjMap.end()){
+        cout << "No multimedia object found with that name." << endl;
+    } else {
+        (it->second)->print(std::cout);
+    }
+}
+
+void Database::printGroupFromName(string name)
+{
+    auto it=groupMap.find(name);
+    if (it == groupMap.end()){
+        cout << "No multimedia object found with that name." << endl;
+    } else {
+        (it->second)->print();
+    }
+}
---
+++ Database.h
@@ -0,0 +1,33 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <map>
+#include <memory>
+#include <list>
+#include <iostream>
+#include "MultimediaObject.h"
+#include "Group.h"
+#include "Film.h"
+#include "VideoObject.h"
+#include "PhotoObject.h"
+
+class Database
+{
+private:
+    map<string, shared_ptr<MultimediaObject>> mObjMap;
+    map<string, shared_ptr<Group>> groupMap;
+
+public:
+    Database();
+    ~Database();
+    shared_ptr<PhotoObject> newPhoto(string _name, string _path, float _lat, float _longit);
+    shared_ptr<VideoObject> newVideo(string _name, string _path, int _duration);
+    shared_ptr<Film> newFilm(string _name, string _path, int _duration, int _nChs, int *_chList);
+    shared_ptr<Group> newGroup(string _name);
+
+    void reproduceFromName(string name);
+    void printObjectFromName(string name);
+    void printGroupFromName(string name);
+};
+
+#endif // DATABASE_H
