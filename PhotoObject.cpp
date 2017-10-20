e90de5e4b2e6d2ff08d276f51618e39172f04ff0
dgallitelli <davidegallitelli@gmail.com>
2017-10-20 12:04:06 +0200

    Doxygen documentation for PhotoObject

--- PhotoObject.cpp
+++ PhotoObject.cpp
@@ -1,25 +1,42 @@
 #include "PhotoObject.h"
 
+/**
+ * @brief Getter method for the latitude variable
+ * @return latitude of the photo
+ */
 float PhotoObject::getLatitude() const
 {
     return latitude;
 }
-
+/**
+ * @brief Setter method for the latitude variable
+ * @param value integer latitude of the photo
+ */
 void PhotoObject::setLatitude(float value)
 {
     latitude = value;
 }
-
+/**
+ * @brief Getter method for the longitude variable
+ * @return longitude of the photo
+ */
 float PhotoObject::getLongitude() const
 {
     return longitude;
 }
-
+/**
+ * @brief Setter method for the longitude variable
+ * @param value integer longitude of the photo
+ */
 void PhotoObject::setLongitude(float value)
 {
     longitude = value;
 }
 
+/**
+ * @brief PhotoObject::reproduce
+ * Reproduce the photo viq the mpv command
+ */
 void PhotoObject::reproduce()
 {
     string command = "mpv "+getPath()+" &";
--- PhotoObject.h
+++ PhotoObject.h
@@ -3,14 +3,35 @@
 
 #include "MultimediaObject.h"
 
+/**
+ * @brief The PhotoObject class
+ * Handler for Photo objects.
+ * Contains methods to set latitude and longitude of the photo, as well as method for its reproduction.
+ */
+
 class PhotoObject : public MultimediaObject
 {
     friend class Database;
 private:
-    float latitude = 0;
-    float longitude = 0;
+    float longitude = 0;    /**< latitude of the photo */
+    float latitude = 0;     /**< longitude of the photo */
+    /**
+     * @brief PhotoObject constructor
+     */
     PhotoObject() {}
+    /**
+     * @brief PhotoObject constructor
+     * @param _name name of the photo object
+     * @param _pathname full path to the photo file
+     */
     PhotoObject(string _name, string _pathname) : MultimediaObject(_name, _pathname){}
+    /**
+     * @brief PhotoObject constructor
+     * @param _name name of the photo object
+     * @param _pathname full path to the photo file
+     * @param lat latitude of the photo
+     * @param longit longitude of the photo
+     */
     PhotoObject(string _name, string _path, float lat, float longit) : MultimediaObject(_name, _path), latitude(lat), longitude(longit) {}
 
 public:
