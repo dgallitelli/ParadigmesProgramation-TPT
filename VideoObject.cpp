c2f1507539b0b6da6a5a67e3d2f4262544ace338
dgallitelli <davidegallitelli@gmail.com>
2017-10-20 12:16:48 +0200

    Doxygen documentation for VideoObject

--- VideoObject.cpp
+++ VideoObject.cpp
@@ -1,15 +1,27 @@
 #include "VideoObject.h"
 
+/**
+ * @brief Getter method for the duration variable
+ * @return An integer with the duration of the video
+ */
 int VideoObject::getDuration() const
 {
     return duration;
 }
 
+/**
+ * @brief Setter method for the duration variable
+ * @param value integer for the duration of the movie
+ */
 void VideoObject::setDuration(int value)
 {
     duration = value;
 }
 
+/**
+ * @brief Method to reproduce the movie
+ * It uses the mpv command.
+ */
 void VideoObject::reproduce()
 {
     string command = "mpv "+getPath()+" &";
--- VideoObject.h
+++ VideoObject.h
@@ -3,14 +3,34 @@
 
 #include "MultimediaObject.h"
 
+/**
+ * @brief The VideoObject class
+ * Handler for Video objects.
+ * Contains methods for information about the video, as well as method for its reproduction.
+ */
+
 class VideoObject : public MultimediaObject
 {
     friend class Database;
 private:
-    int duration = 0;
+    int duration = 0; /**< Duration of the movie */
 protected:
+    /**
+     * @brief VideoObject constructor
+     */
     VideoObject() {}
+    /**
+     * @brief VideoObject constructor
+     * @param _name string containing the name of the video
+     * @param _path string containing the full path of the video
+     */
     VideoObject(string _name, string _path) : MultimediaObject(_name, _path){}
+    /**
+     * @brief VideoObject constructor
+     * @param _name string containing the name of the video
+     * @param _path string containing the full path of the video
+     * @param _duration integer value for the duration of the video
+     */
     VideoObject(string _name, string _path, int _duration) : MultimediaObject(_name, _path), duration(_duration){}
 public:
     ~VideoObject() {}
