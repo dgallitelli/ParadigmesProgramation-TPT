7517369535cda279e68589b587d6005586d79de2
Davide Gallitelli <davidegallitelli@gmail.com>
2017-10-24 14:43:05 +0200

    Implemented AbstractActions

--- ClientJava/ClientMain.java
+++ ClientJava/ClientMain.java
@@ -7,16 +7,21 @@ public class ClientMain extends JFrame{
     private static final long serialVersionUID = 1L;
 
     JTextArea myTextBox;
-    JButton b1, b2, b3;
     JPanel jp;
     JScrollPane jsp;
+    JMenuBar jmb;
+    JToolBar jtb;
 
     public static void main(String argv[]){
-
         new ClientMain();
     }
 
     public ClientMain(){
+        // Declaration
+        JButton b1, b2, b3;
+        JMenu jmActions;
+        JMenuItem jmiB1, jmiB2, jmiB3;
+
         // Initialization
         jp = new JPanel();
         b1 = new JButton("Button1");
@@ -24,12 +29,23 @@ public class ClientMain extends JFrame{
         b3 = new JButton("Button3");
         myTextBox = new JTextArea("Hello World!\n", 3, 25);
         jsp = new JScrollPane(myTextBox);
+        // MenuBar
+        jmb = new JMenuBar();
+        jmActions = new JMenu("Actions");
+        jmiB1 = new JMenuItem(new AbstractButton1("B1"));
+        jmiB2 = new JMenuItem(new AbstractButton2("B2"));
+        jmiB3 = new JMenuItem(new AbstractButton3("Exit"));
+        // ToolBar
+        jtb = new JToolBar();
 
-        // add components to the controlPane
+        // Add components to the JFrame
+        setJMenuBar(jmb); add(jtb);
         add(jp, BorderLayout.SOUTH); add(jsp);
         jp.add(b1); jp.add(b2); jp.add(b3);
+        jmb.add(jmActions); jmActions.add(jmiB1); jmActions.add(jmiB2); jmActions.add(jmiB3);
 
         // Setup listeners
+        /*
         b1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 myTextBox.append("Hello from Button 1!\n");
@@ -44,13 +60,41 @@ public class ClientMain extends JFrame{
             public void actionPerformed(ActionEvent e){
                 System.exit(0);
             }
-        });
+        });*/
+        b1.addActionListener(new AbstractButton1("B1"));
+        b2.addActionListener(new AbstractButton2("B2"));
+        b3.addActionListener(new AbstractButton3("Exit"));
 
         // Run the code
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Top-Box Client");
         pack();
         setVisible(true);
+    }
+
+    public class AbstractButton1 extends AbstractAction{
+        AbstractButton1(String text){
+            super(text);
+        }
+        public void actionPerformed(ActionEvent e){
+            myTextBox.append("Hello from Button 1!\n");
+        }
+    }
+    public class AbstractButton2 extends AbstractAction{
+        AbstractButton2(String text){
+            super(text);
+        }
+        public void actionPerformed(ActionEvent e){
+            myTextBox.append("Hello from Button 2!\n");
+        }
+    }
+    public class AbstractButton3 extends AbstractAction{
+        AbstractButton3(String text){
+            super(text);
+        }
+        public void actionPerformed(ActionEvent e){
+            System.exit(0);
+        }
     }
 
 }
