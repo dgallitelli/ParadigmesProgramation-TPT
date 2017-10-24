ef1af824fd1cb0d1db2f0403a4f702073fa322f2
dgallitelli <davidegallitelli@gmail.com>
2017-10-24 11:02:00 +0200

    Starting work on JavaSwing TP

---
+++ ClientJava/ClientMain.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ClientMain extends JFrame{
+
+    public static void main(String argv[]){
+        
+    }
+
+    public ClientMain(){
+
+    }
+
+
+
+}
