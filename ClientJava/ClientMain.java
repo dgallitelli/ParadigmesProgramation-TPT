70cd7229692d5521e4116e231a57674626a9d87e
dgallitelli <davidegallitelli@gmail.com>
2017-10-31 10:41:32 +0100

    Completed ClientJava

--- ClientJava/ClientMain.java
+++ ClientJava/ClientMain.java
@@ -5,18 +5,47 @@ import java.awt.event.*;
 public class ClientMain extends JFrame{
 
     private static final long serialVersionUID = 1L;
+	static final String HOST = "localhost";
+	static final int PORT = 3331;
+	private static Client client = null;
 
+	JTextArea textArea = new JTextArea(10, 20);
+	JButton search, play, close;
+	JPanel panel;
+	JScrollPane scroll;
+	JTextField textField;
+	JLabel label;
+
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menu, edit, media = new JMenu();
+	JToolBar toolBar = new JToolBar();
+
+    /*
     JTextArea myTextBox;
     JPanel jp;
     JScrollPane jsp;
     JMenuBar jmb;
-    JToolBar jtb;
+    JToolBar jtb;*/
 
     public static void main(String argv[]){
         new ClientMain();
+    	String host = HOST;
+    	int port = PORT;
+    	if (argv.length >= 1) host = argv[0];
+    	if (argv.length >= 2) port = Integer.parseInt(argv[1]);
+
+    	try {
+    		client = new Client(host, port);
+    	} catch (Exception e) {
+    		System.err.println("Client: Couldn't connect to "+host+":"+port);
+    	    System.exit(1);
+    	}
+
+        System.out.println("Client connected to "+host+":"+port);
     }
 
     public ClientMain(){
+        /*
         // Declaration
         JButton b1, b2, b3;
         JMenu jmActions;
@@ -49,7 +78,6 @@ public class ClientMain extends JFrame{
         jmb.add(jtb);
 
         // Setup listeners
-        /*
         b1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 myTextBox.append("Hello from Button 1!\n");
@@ -64,7 +92,7 @@ public class ClientMain extends JFrame{
             public void actionPerformed(ActionEvent e){
                 System.exit(0);
             }
-        });*/
+        });
         b1.addActionListener(new AbstractButton1("B1"));
         b2.addActionListener(new AbstractButton2("B2"));
         b3.addActionListener(new AbstractButton3("Exit"));
@@ -99,6 +127,62 @@ public class ClientMain extends JFrame{
         public void actionPerformed(ActionEvent e){
             System.exit(0);
         }
-    }
+    }*/
+		setLayout(new BorderLayout());
+
+		add(textArea, BorderLayout.CENTER);
+		add(scroll = new JScrollPane(textArea));
+
+		add(panel = new JPanel(), BorderLayout.SOUTH);
+
+		panel.add(label = new JLabel("Object name: "));
+		panel.add(textField = new JTextField(20));
+		panel.add(search = new JButton("Search"));
+		panel.add(play = new JButton("Play"));
+		panel.add(close = new JButton("Close"));
+
+		search.addActionListener(new SearchListener());
+		play.addActionListener(new PlayListener());
+		close.addActionListener(new CloseListener());
+
+		add(toolBar, BorderLayout.NORTH);
+		setJMenuBar(menuBar);
+		menuBar.add(menu = new JMenu("Menu"));
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+
+	class SearchListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String name = "info " + textField.getText();
+			String s = client.send(name) + "\n";
+			textArea.append(s);
+		}
+
+	}
+
+	class PlayListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String name = "play " + textField.getText();
+			String s = client.send(name) + "\n";
+			textArea.append(s);
+		}
+
+	}
+
+	class CloseListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+
+	}
 
 }
